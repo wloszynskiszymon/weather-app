a7eef50f1024676d74cbfc37897a42075a293c85
wloszynskiszymon <ace3szymon@gmail.com>
2022-09-13 14:21:21 +0200

    Fixing nav bug, implementing search input

--- src/js/app.js
+++ src/js/app.js
@@ -1,33 +1,16 @@
 // Needed for nav to work
 const nav = document.querySelector(".nav__circle-checkbox");
 const navBg = document.querySelector(".nav__circle-background");
-const form = document.querySelector(".form");
+const form = document.querySelector(".theme-form");
 
-let isNavActive = false;
+let isNavOpened = false;
 
 nav.addEventListener("change", () => {
-  // form.classList.toggle("hide-form");
-
-  // Checking if the nav is opened or not.
-  if (!isNavActive) {
-    navBg.removeEventListener("transitionstart", hideForm);
-    navBg.addEventListener("transitionend", showForm);
+  if (!isNavOpened) {
+    form.classList.toggle("theme-form--show");
+    isNavOpened = true;
   } else {
-    navBg.removeEventListener("transitionend", showForm);
-    navBg.addEventListener("transitionstart", hideForm);
+    form.classList.toggle("theme-form--show");
+    isNavOpened = false;
   }
 });
-
-function hideForm() {
-  form.style.visibility = "hidden";
-  form.style.opacity = "0";
-  form.style.transform = "translateY(100rem) scale(0.3)";
-  isNavActive = false;
-}
-
-function showForm() {
-  form.style.visibility = "visible";
-  form.style.opacity = "1";
-  form.style.transform = "translateY(0) scale(1)";
-  isNavActive = true;
-}
