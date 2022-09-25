5c74ddec3ebfc3c62b6a4f73947b92c4d16c9c35
wloszynskiszymon <ace3szymon@gmail.com>
2022-09-25 14:14:44 +0200

    Refactoring class names, refactoring scss files, adding simple working navigation between layouts, adding small bux-fixes

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,84 @@
 import { getJSON } from "./requestAPI";
 // Needed for nav to work
-const nav = document.querySelector(".nav__circle-checkbox");
-const navBg = document.querySelector(".nav__circle-background");
-const form = document.querySelector(".theme-form");
+const navCircle = document.querySelector(".nav__circle");
+const navCricleCheckbox = document.querySelector(".nav__circle-checkbox");
+const navArrow = document.querySelector(".nav__arrow");
+const themeForm = document.querySelector(".theme");
+const searchForm = document.querySelector(".search");
+const searchLoadedForm = document.querySelector(".search-loaded");
+const weatherInfo = document.querySelector(".info__container");
+
+// That's "See all possible themes" paragraph
+const searchCityIcon = document.querySelector(".search__input__icon");
+const navBackSpan = document.querySelector(".nav__back span");
+const navLink = document.querySelector(".nav__link");
+
 // For weather info
-const infoTemperature = document.querySelector(".info__container-temperature");
+const background = document.querySelector("#background");
 
-let isNavOpened = false;
+// Search
 
-nav.addEventListener("change", () => {
-  if (!isNavOpened) {
-    form.classList.toggle("theme-form--show");
-    isNavOpened = true;
+const state = {
+  background: "first-entry",
+  weather: "snowy",
+  timeOfDay: "night",
+  loadedCity: "false",
+  themeNavOpened: "false",
+};
+
+const changeView = function () {
+  searchForm.classList.toggle("search--hidden");
+  navArrow.classList.toggle("nav__arrow--hidden");
+  searchLoadedForm.classList.toggle("search-loaded--hidden");
+  weatherInfo.classList.toggle("info--hidden");
+  if (state.loadedCity) {
+    background.classList.remove(state.background);
+    state.background = generateBgClass();
+    background.classList.add(state.background);
+    state.loadedCity = !state.loadedCity;
+    return;
+  }
+
+  background.classList.remove(state.background);
+  state.background = "first-entry";
+  background.classList.add(state.background);
+  state.loadedCity = !state.loadedCity;
+  return;
+};
+
+const generateBgClass = function () {
+  return `${state.weather}-${state.timeOfDay}`;
+};
+
+// When searching a city via first load
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  changeView();
+});
+
+searchLoadedForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+navArrow.addEventListener("click", changeView);
+
+// "See all possible themes"
+navCricleCheckbox.addEventListener("change", () => {
+  if (!state.themeNavOpened) {
+    themeForm.classList.toggle("theme--show");
+    state.themeNavOpened = true;
   } else {
-    form.classList.toggle("theme-form--show");
-    isNavOpened = false;
+    themeForm.classList.toggle("theme--show");
+    state.themeNavOpened = false;
   }
 });
+
+[navLink, navBackSpan].forEach((el) =>
+  el.addEventListener("click", () => {
+    navCircle.classList.toggle("nav__circle--hidden");
+    searchForm.classList.toggle("search--hidden");
+    navBackSpan.classList.toggle("hidden");
+  })
+);
 
 // getJSON("Poznan");
