44b1b6741ec722893970374d535e66ae60dd0d1f
wloszynskiszymon <ace3szymon@gmail.com>
2022-10-16 00:03:18 +0200

    Fixing stars glitching, rainy clouds glitching and AM/PM format causing background bugs in time counting

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -3,5 +3,9 @@ export const DATE_FORMAT = {
   month: "short",
   day: "numeric",
 };
-export const TIME_FORMAT = { hour: "2-digit", minute: "2-digit" };
+export const TIME_FORMAT = {
+  hour: "2-digit",
+  minute: "2-digit",
+  hour12: false,
+};
 export const API_KEY = "5614087c1b4e3b8884b8ec32fd8a2151";
